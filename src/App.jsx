8dd441474d691002d9cf7999c949c2c165b2ab40
Firdavs <firdavsrixsiboev577@gmail.com>
2025-01-13 19:57:29 +0500

    navbar-logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/navbar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    salom
+    <Navbar/>
     </>
   )
 }
